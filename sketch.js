7bc173335fed3e34b5405c005a808e3071674f60
VivianChan1998 <b05901179@ntu.edu.tw>
2022-02-25 16:37:56 +0800

    fix layering

--- sketch.js
+++ sketch.js
@@ -6,15 +6,17 @@ const locList = [ "chairL", "chairR", "whiteBoardL", "whiteBoardR", "tableL", "t
 const locXY = [[20,250], [250,250], [350,200], [460,200], [430,260], [570,200], [730,250], [160,180], [650,180]]
 let loc = [0,1,2,3,4,5,6,7,8]  // index as char id, loc[1] ==> bi's loc  //last one is dummy
 let locschar = [0, 0, 0, 0, 0, 0, 0, 0, 0] //index as loc id, locschar[0] ==> who's in chairL // val==8 ==> no one there
+const frontLoc = [0,1,4,6]
+const backLoc = [2,3,5,7,8]
+
+let isHere = [0, 0, 0, 0, 0, 0, 0, 0]
+shuffle(loc)
 for (var i=0; i<9; ++i)
 {
     locschar[loc[i]] = i
 }
-
-
-let isHere = [0, 0, 0, 0, 0, 0, 0, 0]
-shuffle(loc)
 console.log(loc)
+console.log(locschar)
 
 let All = []
 let script = []
@@ -71,13 +73,35 @@ function draw()
 
     //background
     animation(explode_animation, 450, 250);
+
     //blackboard
     if (blackboardID < 5) image(blackboard[blackboardID], 0, 0, 900, 500)
+
+    for (var i=0; i<backLoc.length; ++i)
+    {
+        let who = locschar[backLoc[i]]
+        if (who != 8 && isHere[who])
+        {
+            let l = loc[who]
+            image(img[who][l], 0, 0, 900, 500)
+        }
+    }
 
     //tables
     image(table, 0, 0, 900, 500)
 
     //members
+    
+    for (var i=0; i<frontLoc.length; ++i)
+    {
+        let who = locschar[frontLoc[i]]
+        if (who != 8 && isHere[who])
+        {
+            let l = loc[who]
+            image(img[who][l], 0, 0, 900, 500)
+        }
+    }
+    /*
     for(var i=0; i<8; ++i)
     {
         if(isHere[i])
@@ -87,7 +111,7 @@ function draw()
             //onScreenImg.mouseOver(() => console.log(script[i][locList[l]][0]))
         }
     }
-    
+    */
 
 }
 
