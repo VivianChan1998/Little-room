ed4d94accbd68bb54a407e724ffe0be9f11becf1
VivianChan1998 <b05901179@ntu.edu.tw>
2022-02-25 17:15:10 +0800

    add timing

--- sketch.js
+++ sketch.js
@@ -7,16 +7,10 @@ const locXY = [[20,250], [250,250], [350,200], [460,200], [430,260], [570,200], 
 let loc = [0,1,2,3,4,5,6,7,8]  // index as char id, loc[1] ==> bi's loc  //last one is dummy
 let locschar = [0, 0, 0, 0, 0, 0, 0, 0, 0] //index as loc id, locschar[0] ==> who's in chairL // val==8 ==> no one there
 const frontLoc = [0,1,4,6]
-const backLoc = [2,3,5,7,8]
+const backLoc = [2,3,7,8,5]
 
 let isHere = [0, 0, 0, 0, 0, 0, 0, 0]
-shuffle(loc)
-for (var i=0; i<9; ++i)
-{
-    locschar[loc[i]] = i
-}
-console.log(loc)
-console.log(locschar)
+setLoc()
 
 let All = []
 let script = []
@@ -24,6 +18,13 @@ let img = []
 let blackboard = []
 let blackboardID = 0
 let table = ''
+
+let t_m = 0
+let t_h = 0
+let resetFlag = false
+let clock;
+let hourhand;
+let minutehand;
 
 function preload()
 {
@@ -49,6 +50,9 @@ function preload()
     }
     blackboardID = Math.floor(Math.random() * 10) 
     loadImage('./src/background/front.png', e => table = e)
+    loadImage('./src/background/clock.png', e => clock = e)
+    loadImage('./src/background/hour.png', e => hourhand = e)
+    loadImage('./src/background/minute.png', e => minutehand = e)
 }
 
 function loadNext (j, temp, img, mem)
@@ -64,12 +68,14 @@ function setup()
 {
     createCanvas(900, 500);
     frameRate(10);
+    t_m = minute()
+    t_h = hour()
+    let timeoutID = window.setInterval(( () => resetScene() ), 60000 * 2);
 }
 
 function draw()
 {
     clear();
-    line(0, 0, 900, 500); //temp
 
     //background
     animation(explode_animation, 450, 250);
@@ -77,6 +83,7 @@ function draw()
     //blackboard
     if (blackboardID < 5) image(blackboard[blackboardID], 0, 0, 900, 500)
 
+    //members in back
     for (var i=0; i<backLoc.length; ++i)
     {
         let who = locschar[backLoc[i]]
@@ -90,8 +97,7 @@ function draw()
     //tables
     image(table, 0, 0, 900, 500)
 
-    //members
-    
+    //members in front
     for (var i=0; i<frontLoc.length; ++i)
     {
         let who = locschar[frontLoc[i]]
@@ -101,22 +107,32 @@ function draw()
             image(img[who][l], 0, 0, 900, 500)
         }
     }
-    /*
-    for(var i=0; i<8; ++i)
-    {
-        if(isHere[i])
-        {
-            let l = loc[i]
-            let onScreenImg = image(img[i][l], 0, 0, 900, 500)
-            //onScreenImg.mouseOver(() => console.log(script[i][locList[l]][0]))
-        }
-    }
-    */
 
+    //time system
+    /*t_m = minute()
+    if(t_m % 2 == 0 && !resetFlag) resetScene()
+    else if (t_m % 2 != 0) resetFlag = false
+    console.log(t_m)
+    */
+    
 }
 
-function resetScene() {
+function resetScene()
+{
     blackboardID = Math.floor(Math.random() * 10) 
+    setLoc()
+    resetFlag = true
+}
+
+function setLoc()
+{
+    console.log(loc)
+    loc = shuffle(loc)
+    for (var i=0; i<9; ++i)
+    {
+        locschar[loc[i]] = i
+    }
+    console.log(loc)
 }
 
 function shuffle(array) {
@@ -124,4 +140,5 @@ function shuffle(array) {
       let j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
+    return array
 }
